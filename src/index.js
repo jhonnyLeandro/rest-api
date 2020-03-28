ec080cc22b1777bad2c8478fb1e490eca630d728
test <tset>
2020-03-28 17:57:39 -0500

    Decode response json and show img

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const request = require('request');
 
 request('https://xkcd.com/info.0.json', (err, res) => {
     if (err) { return console.log(err); }
-    console.log(res.body);
+    console.log(JSON.parse(res.body).img);
 });
