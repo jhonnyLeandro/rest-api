eb48b99f0b3126e25d54398e3ff4aedb735b5754
johhnny escobar <ejohhnny@gmail.com>
2020-03-30 20:46:40 -0500

    Change code from spanish to english and add a list of users

--- src/api_rest_example/service.js
+++ src/api_rest_example/service.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require('body-parser');
-
+const port = 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -8,142 +8,100 @@ app.use(bodyParser.json());
 
 
 
-let usuario = {
-    nombre:'',
-    apellido: ''
+let users = [];
+let user = {
+    name: "",
+    lastname:""
 };
 
-let respuesta = {
+let response = {
     error: false,
-    codigo: 200,
-    mensaje: ''
+    code: 200,
+    message: ''
 };
 
+// get to answer to the root
 app.get('/', (req, res) => {
-    respuesta = {
+    response = {
         error: true,
-        codigo: 200,
-        mensaje: 'Punto de inicio'
+        code: 200,
+        message: 'Punto de inicio'
     };
-    res.send(respuesta);
+    res.send(response);
+});
+
+// get to obtain all the users
+app.get('/users',(req, res) => {
+    res.send(users);
+})
+
+// get to obtain a specific user
+app.get('/user/:index', (req, res) => {
+    
+    let index = req.params.index;
+    if (index >= users.length){
+        res.send({"error":"There isn't user with specified index"})
+    }
+    res.send(users[index]);
+});
+
+//post to add a user
+app.post('/user', (req, res) => {
+    if(!req.body.name || !req.body.lastname) {
+        res.send ({"error": "name and last name fields required"});
+    } else {
+
+        user = {
+            name: req.body.name,
+            lastname: req.body.lastname
+        };
+
+        users.push (user);
+
+        response = {
+            error: false,
+            code: 200,
+            message: 'user created',
+            response: users
+        };
+    } 
+    res.send(response);
+});
+
+app.put('/user/:index', (req, res) => {
+    if (!req.body.name || !req.body.lastname) {
+        res.send ({"error": "name and last name fields required"});
+    } else {
+        let index = req.params.index;
+        if (index >= users.length) {
+            res.send({"error":"There isn't user with specified index"})
+        }
+        users[index].name = req.body.name;
+        users[index].lastname = req.body.lastname;
+    }
+
+    res.send({"message":"Operation completed successfully"});
 });
 
 
-app.route('/usuario')
-.get((req, res) => {
-    respuesta = {
-        error: false,
-        codigo: 200,
-        mensaje: ''
-    };
-
-    if(usuario.nombre == '' || usuario.apellido == '') {
-        respuesta = {
-            error: true,
-            codigo: 501,
-            mensaje: 'El usuario no ha sido creado'
-        };
-    } else {
-        respuesta = {
-            error: false,
-            codigo: 200,
-            mensaje: 'respuesta del usuario',
-            respuesta: usuario
-        };
+app.delete ('/user/:index', (req, res) => {
+    let index = req.params.index;
+    if (index >= users.length) {
+        res.send ({"error":"There isn't user with specified index"});
     }
-    res.send(respuesta);
-})
-.post((req, res)=> {
-    if(!req.body.nombre || !req.body.apellido) {
-        respuesta = {
-            error: true,
-            codigo: 502,
-            mensaje: 'El campo nombre y apellido son requeridos'
-        };
-    } else {
-        if( usuario.nombre != '' || usuario.apellido != '' ) {
-            respuesta = {
-                error: true,
-                codigo: 503,
-                mensaje: 'El usuario ya fue creado previamente'
-            };
-        } else {
-            usuario = {
-                nombre: req.body.nombre,
-                apellido: req.body.apellido
-            };
-
-            respuesta = {
-                error: false,
-                codigo: 200,
-                mensaje: 'Usuario creado',
-                respuesta: usuario
-            };
-        }
-    }
-        
-    res.send(respuesta);
-})
-.put( (req, res) => {
-    if (!req.body.nombre || !req.body.apellido) {
-        respuesta = {
-            error: true,
-            codigo: 502,
-            mensaje: 'El campo nombre y apellido son requeridos'
-        };
-    } else {
-        if ( usuario.nombre == '' || usuario.apellido == '') {
-            respuesta = {
-                error: true,
-                codigo: 501,
-                mensaje: 'El usuario no ha sido creado'
-            };
-        } else {
-            usuario = {
-                nombre: req.body.nombre,
-                apellido: req.body.apellido
-            };
-            respuesta = {
-                error: false,
-                codigo: 200,
-                mensaje: 'Usuario actualizado',
-                respuesta: usuario
-            };
-        }
-    }
-
-    res.send(respuesta);
-})
-.delete( (req, res) => {
-    if ( usuario.nombre == '' || usuario.apellido == '') {
-        respuesta = {
-            error: true,
-            codigo: 501,
-            mensaje: 'El usuario no ha sido creado'
-        };
-    } else {
-        respuesta = {
-            error: false,
-            codigo: 200,
-            mensaje: 'Usuario eliminado'
-        };
-        usuario = { 
-            nombre: '', 
-            apellido: '' 
-        };
-    }
-    res.send(respuesta);
+    delete users[index];
+    res.send(response);
 });
 
 app.use(function(req, res, next) {
-    respuesta = {
+    rsp = {
         error: true, 
-        codigo: 404, 
-        mensaje: 'URL no encontrada'
+        code: 404, 
+        message: 'URL not found'
     };
-    res.status(404).send(respuesta);
+    res.status(404).send(response);
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server initialized, listening port 3000");
 });
