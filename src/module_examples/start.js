7165f7e26bdbeb2a79b123fe213fe7b472ca65cb
test <tset>
2020-04-16 21:24:17 -0500

    Arrange the project structure

--- src/module_examples/start.js
+++
@@ -1,8 +0,0 @@
-const hello = require ('./bar').hello;
-const awesome = require ('./foo').awesome; 
-
-console.log(
-	hello ( "rhino" )
-); // Let me introduce: rhino
-
-awesome ();
--- src/request_examples/simple_request.js
+++
@@ -1,7 +0,0 @@
-const request = require('request'),
-url = 'https://xkcd.com/info.0.json' ;
-
-request(url, (err, res) => {
-    if (err) { return console.log(err); }
-    console.log(JSON.parse(res.body).img);
-});
