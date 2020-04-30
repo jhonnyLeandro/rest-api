3aa686407b9c53f3a89591dbcea2350d54aec018
jhonny-leandro <ejohhnny@gmail.com>
2020-04-30 11:10:49 -0700

    Remove code to create model and add it to other file

--- src/orm/sequalizeconnection.js
+++ src/orm/sequalizeconnection.js
@@ -1,5 +1,4 @@
 const Sequelize = require('sequelize');
-const Model = Sequelize.Model;
 
 let db = process.env.DB;
 let user = process.env.USR;
@@ -10,39 +9,4 @@ const sequelize = new Sequelize(db, user, password, {
   dialect:  'postgres'
 });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-
-  class User extends Model {}
-
-  User.init({
-    // attributes
-    firstName: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    lastName: {
-      type: Sequelize.STRING
-      // allowNull defaults to true
-    }
-  }, {
-    sequelize,
-    modelName: 'user'
-    // options
-  });
-
-  // Note: using `force: true` will drop the table if it already exists
-User.sync({ force: true }).then(() => {
-    // Now the `users` table in the database corresponds to the model definition
-    return User.create({
-      firstName: 'John',
-      lastName: 'Hancock'
-    });
-  });
+module.exports.connection = sequelize;
