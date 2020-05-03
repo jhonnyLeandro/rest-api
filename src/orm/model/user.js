e965a4f0df04f87965ab241f9ce3514b08981bc9
jhonny-leandro <ejohhnny@gmail.com>
2020-05-02 20:13:33 -0700

    Update way in which user model is created

--- src/orm/model/user.js
+++ src/orm/model/user.js
@@ -1,17 +1,25 @@
-const Sequelize = require('sequelize');
-const Model = Sequelize.Model;
-const connection = require('../sequalizeconnection');
-class User extends Model {};
+import Sequelize from 'sequelize';
 
-User.init({
-  firstName: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  lastName: {
-    type: Sequelize.STRING
-  }
-}, {
-  sequelize,
-  modelName: 'user'
-});
+function createUser (sequelize) {
+  
+  const Model = Sequelize.Model;
+
+  class User extends Model {};
+
+  User.init({
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: Sequelize.STRING
+    }
+  }, {
+    sequelize,
+    modelName: 'user'
+  });
+
+  return User;
+}
+
+export default createUser;
